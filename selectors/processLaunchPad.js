169a7d989fdb143af5ada3dffe35f2a7f3c132f0
ana <ana.mauricio@processmaker.com>
2024-04-26 11:17:40 -0400

    Add some fixes to launch pad page and selectors

--- selectors/processLaunchPad.js
+++ selectors/processLaunchPad.js
@@ -1,18 +1,19 @@
 export default {
     //Settings
     settings_descriptions: '[aria-label="Description"]',
-    settings_selectListIcon: '(//*[@class="multiselect__tags"])[1]',
-    settings_inputIcon: '(//*[@class="multiselect__tags"])[1]//input',
+    settings_selectListIcon: '(//*[@class="multiselect__tags"])[2]',
+    settings_inputIcon: '(//*[@class="multiselect__tags"])[2]//input',
     settigns_textIcon: '//*[@class="modal-content"]//*[contains(@class,"multiselect-icons")]//span/span',
     settings_uploadImage: '//input[@type="file"]',
     settings_saveConfiguration: '//button[contains(text(),"Save")]',
     settings_cancelConfiguration: '//*[contains(@class,"footer")]//button[contains(text(),"Cancel")]',
     settings_versionInfoButton: '//button[contains(., "Version")]',
     settings_launchPadButton: '//button[contains(., "Launchpad")]',
+    settings_dragButton: '[id="dropdownMenuUpload"]',
     addReports: '//button[@aria-label="Add Reports"]',
     publishSuccessAlert: '[class="alert d-none d-lg-block alertBox alert-dismissible alert-success"]',
 
-    launch_category: '//*[contains(text(),"category")]',
+    launch_category: '//*[@class="menu"]//*[contains(text(),"category")]',
     launch_searchProcess: '[id="search-box"][placeholder="Search Processes"]',
     launch_searchProcessButton: '[title="Search Processes"]',
     launch_cardProcess: '//*[contains(text(),"processName")]',
@@ -27,6 +28,7 @@ export default {
     launchPadSettingOption: '//a[contains(., "Launchpad Settings")]',
     inputSearchCategories: '//input[@placeholder="Search Categories"]',
     categoryFound: "//div['#infinite-list']/div[contains(text(),'category')]",
-    bookMarkedSuccessAlert: '[class="alert d-none d-lg-block alertBox alert-dismissible alert-success"]'
+    bookMarkedSuccessAlert: '[class="alert d-none d-lg-block alertBox alert-dismissible alert-success"]',
+    optionLaunchPad: '//a//span[contains(text(),"option")]'
 
 }
