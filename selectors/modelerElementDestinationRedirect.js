9f3477f6fd0d73dad7cbc2ce06dd32431bd43ce9
WendyFPM <wendy.chuquimia@processmaker.com>
2024-07-02 09:18:51 -0400

    AddMethodsToMEDR

--- selectors/modelerElementDestinationRedirect.js
+++ selectors/modelerElementDestinationRedirect.js
@@ -11,4 +11,5 @@ export default {
     processInput:'input[name="Process"]',
     selectProcess:'//label[text()="Process"]/following-sibling::div//div[@class="multiselect__select"]',
     wrapperProcess:'//label[text()="Process"]/following-sibling::div//div[@class="multiselect__content-wrapper"]//li[1]',
+    optionsInElementDestination:'//div[@data-test="element-destination-type"]//ul//li[@role="option"]',
 }
