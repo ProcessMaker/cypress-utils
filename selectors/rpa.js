b9a12ae845afb64e724639452ad00c9db8dee442
PaolaPellegrini <51709910+PaolaPellegrini@users.noreply.github.com>
2024-07-04 13:37:52 -0400

    new functions for RPA

---
+++ selectors/rpa.js
@@ -0,0 +1,13 @@
+export default {
+
+    searchItemRPA: '//textarea[@type="text"][@placeholder="Search here"]',
+    itemOrganizationName: '//*[@class="settings-listing data-table"]//div[contains(text(),"Organization Name")]/ancestor::tr/td[@aria-colindex="1"]',
+    itemClientID: '//*[@class="settings-listing data-table"]//div[contains(text(),"Client ID")]/ancestor::tr/td[@aria-colindex="1"]',
+    itemClientSecret: '//*[@class="settings-listing data-table"]//div[contains(text(),"Client Secret")]/ancestor::tr/td[@aria-colindex="1"]',
+    itemTenantID: '//*[@class="settings-listing data-table"]//div[contains(text(),"Tenant ID")]/ancestor::tr/td[@aria-colindex="1"]',
+    itemSelectAvailableFolders: '//*[@class="settings-listing data-table"]//div[contains(text(),"Select Available Folders")]/ancestor::tr/td[@aria-colindex="1"]',
+
+
+
+
+}
