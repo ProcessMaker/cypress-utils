f5f514d5da171387638be80509a4a074a9310eb0
WendyFPM <wendy.chuquimia@processmaker.com>
2024-07-11 14:43:26 -0400

    methodsToCalcs

---
+++ selectors/calcsAndWatchers.js
@@ -0,0 +1,19 @@
+export default{
+ //CALCS
+    calcsBtn:'[data-cy="toolbar-button_calcs"]',
+    //Modal calcs list
+    searchCalcs: '[data-test="search"]',
+    addCalcsBtn: '[data-cy="calcs-add-property"]',
+    editCalcsBtn: 'button[data-cy="calcs-table-edit"]',
+    bypassCalcsBtn:'button[data-test="calcs-bypass"]',
+    deleteCalcsBtn: 'button[data-cy="calcs-table-remove"]',
+    //Modal create calcs
+    propertyNameField:'[data-cy="calcs-property-name"]',
+    descriptionField:'[data-cy="calcs-property-description"]',
+    formulaField:'[data-cy="calcs-property-formula"]',
+    javaScriptField:'[data-cy="calcs-property-javascript"]',
+    formulaBtn:'[data-cy="calcs-switch-formula"]',
+    javaScriptBtn:'[data-cy="calcs-switch-javascript"]',
+    cancelCalcsBtn:'[data-cy="calcs-button-cancel"]',
+    saveCalcsBtn:'[data-cy="calcs-button-save"]',
+}
