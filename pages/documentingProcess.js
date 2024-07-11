b5866a0fcbd966c8441d9e57b1deb3fffd9a5a15
mayramelisa <mayra.quispe@processmaker.com>
2024-07-11 08:50:18 -0400

    Funtions Documenting

--- pages/documentingProcess.js
+++ pages/documentingProcess.js
@@ -4,7 +4,7 @@ const navHelper = new NavigationHelper();
 
 export class DocumentingProcess {
 
-    //Form modeler
+    //Form modeler add documentation
     objectDocumentation(elementName,elementXpath,documentation){
         cy.xpath(elementXpath.replace('nameElem',elementName)).first().should('be.visible').click();
         cy.get('[data-cy="inspector-button"]').should('be.visible').click();
@@ -17,7 +17,43 @@ export class DocumentingProcess {
             const $body = $iframe.contents().find('body');
             cy.wrap($body).find('p').type(documentation);
         });
-        cy.get('[data-cy="inspector-close-button"]').click().wait(6000);
+        cy.get('[data-cy="inspector-close-button"]').click().wait(5000);
     }
 
+    //Open alternative A/B in documenting 
+    openAlternativeInDocumenting(alternative = "A") {
+        cy.url().then(($url) => {
+            let processID = $url.split("/")[4].trim();
+            cy.visit("/modeler/" + processID + "/documentation/" + alternative + "?generated=false");
+        });
+    }
+
+    //Go to Documentation from modeler
+    goToDocumentationFromModeler() {
+        cy.get(selectors.options).first().should('exist').click();
+        cy.get(selectors.opDumentation).should('be.visible').click();
+    }
+
+    //Click on Document with AI
+    documentWithAI(){
+        cy.get(selectors.docWithAI).first().should('exist').click();
+        cy.xpath(selectors.modalGenerate).should('be.visible');
+    }
+
+    //Select Generate Documentation from Scratch button
+    optionGenerateDoc(){
+        cy.xpath(selectors.generateDocBtn).should('be.visible').click();
+    }
+
+    //Select option Use current Documentation button
+    optionCurrentDoc(){
+        cy.xpath(selectors.useCurrentDocBtn).should('be.visible').click();
+    }
+
+    //Select option Cancel button
+    cancelBtn(){
+        cy.get(selectors.cancelBtn).should('be.visible').click();
+    }
+        
+        
 }    
