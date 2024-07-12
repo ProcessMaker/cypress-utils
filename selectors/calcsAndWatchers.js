381eb6e1b4de1aa8975b8268f95050bee1314f84
WendyFPM <wendy.chuquimia@processmaker.com>
2024-07-12 17:19:21 -0400

    AddsSelectorsAndMethodsIToWatchers

--- selectors/calcsAndWatchers.js
+++ selectors/calcsAndWatchers.js
@@ -17,7 +17,7 @@ export default{
     cancelCalcsBtn:'[data-cy="calcs-button-cancel"]',
     saveCalcsBtn:'[data-cy="calcs-button-save"]',
     //WATCHERS
-    watchersBtn:'[data-test="search"]',
+    watchersBtn:'[data-cy="toolbar-button_watchers"]',
     //Modal watchers list
     searchWatchers: '[data-test="search"]',
     addWacthersBtn: '[data-cy="watchers-add-watcher"]',
@@ -26,4 +26,29 @@ export default{
     deleteWatchersBtn: 'button[data-cy="watchers-table-remove"]',
     cancelDeleteBtn:'button[data-test="confirm-btn-close"]',
     confirmDeleteBtn:'button[data-test="confirm-btn-ok"]',
+    //Modal create watchers
+    //Configuration
+    configurationAccordion:'button[data-cy="watchers-accordion-configuration"]',
+    watcherName:'[data-cy="watchers-watcher-name"]', 
+    variableToWatchlabel:'//label[text()="Variable to Watch *"]',
+    variableToWatchInput:'input[name="Variable to Watch"]',
+    variableToWatchSelect:'//div[@data-cy="watchers-watcher-variable"]//div[@class="multiselect__select"]',
+    variableToWatchWrapper:'//div[@data-cy="watchers-watcher-variable"]//div[@class="multiselect__content-wrapper"]//li[1]',
+    runSynchronouslyBtn:'input[data-cy="watchers-watcher-synchronous"]',
+    showMessageWhileLoadingRemoteDataBtn:'input[data-cy="watchers-watcher-show-loading"]',
+    runWatcherOnScreenLoadBtn:'input[data-cy="watchers-watcher-run-onload"]',
+    //Source
+    sourceAccordion:'button[data-cy="watchers-accordion-source"]',
+    sourcelabel:'//label[text()="Source *"]',
+    sourceInput:'input[name="Source"]',
+    sourceSelect:'//div[@data-cy="watchers-watcher-source"]//div[@class="multiselect__select"]',
+    sourceWrapper:'//div[@data-cy="watchers-watcher-source"]//div[@class="multiselect__content-wrapper"]//li[1]',
+    inputDataField:'[data-cy="watchers-watcher-input_data"]',
+    scriptConfigurationField:'[data-cy="watchers-watcher-script_configuration"]',
+    //Output
+    outputAccordion:'[id="watcherOutput"]',
+    outputVariableField:'input[data-cy="watchers-watcher-output_variable"]',
+    cancelWatchersBtn:'[data-cy="watchers-button-cancel"]',
+    saveWatchersBtn:'[data-cy="calcs-button-save"]',
+    closeModal:'[aria-label="Close"]',
 }
