abc508b7d4507ae98de732487c201875c37dcdc3
pablobarrososso <pablo.barroso@processmaker.com>
2024-09-23 11:26:32 -0400

    commitFunctionsTaskPreviewTane

--- selectors/taskPreviewPane.js
+++ selectors/taskPreviewPane.js
@@ -1,7 +1,8 @@
 export default {
-    taskFilter: '//span[text()="Filter"]',
-    taskFilterRequest:'//input[@id="process_request_filter"]',
-    applyFilterButton: '//button[text()="Apply"]',
+    taskFilter: '//span[text()="Status"]/ancestor::th/div[".pm-table-filter-button"]/div/button',
+    applyFilterButton: '//button[contains(text(),"Apply")]',
+    eyeTaskPreview: "//a[contains(text(),'task')]/ancestor::tr/td/div['.pm-task-row-buttons']/div['.pm-floating-buttons']",
+    eyeButton: '//a[contains(text(),"task")]/ancestor::tr/td/div[".pm-task-row-buttons"]/div[".pm-floating-buttons"]/span/button["#openPreviewButton0"]',
     inProgressRemoveElement:'//span[text()="In Progress"]/ancestor::span/i[@aria-label="Remove Element"]',
     taskFilterStatus: '//input[@id="process_status_options_filter"]',
     labelSelected: "//div/span/span[contains(text(),'label')]"
