5e4163a455da5c51f7623cbcf071d9fb8d628b3e
karlOS123testing <carlosalbertocamachocapriles@gmail.com>
2025-10-23 03:55:41 -0400

    add filteringV1

--- selectors/filtering.js
+++ selectors/filtering.js
@@ -5,4 +5,14 @@ export default {
     filterForm: '[class="pm-filter-form"]',
     filterFooterBtns: '[class="pm-filter-form"] fieldset',
     tableRequest: '[class="pm-table-container"]',
+    //new filteringV1
+    openFilter: '[class="pm-table-ellipsis-column"] [class="pm-table-column-header"]',
+    threeDots: '.pm-table-ellipsis-column button',
+    modalFilter: '[class="pm-filter-form"]',
+    filterForm: '[class="pm-filter-form"]',
+    sortButton: '[class="pm-filter-form"] fieldset',
+    optionFilter: '[data-cy="pmFilterFormArea"] [data-cy="operator{row}"]',
+    valueFilter: '[data-cy="pmFilterFormArea"] [data-cy="value{row}"]',
+    removeButtonModal: '[data-cy="pmFilterFormArea"] [data-cy="buttonRemove{row}"]',
+    addNewFilterButton: '[class="btn pm-filter-form-button btn-light btn-sm"] rect[fill="#D9D9D9"]'
 };
