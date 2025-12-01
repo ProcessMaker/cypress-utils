cb56e0e1910fbdfc8645e2fe629bc8fda809b679
DevAlePM <alejandro.arias@processmaker.com>
2025-12-01 08:27:07 -0400

    Update filtering selectors

--- selectors/filtering.js
+++ selectors/filtering.js
@@ -1,11 +1,11 @@
 export default {
-    openFilter: '[id="pm-table-column-{col}"] button',
+    //openFilter: '[id="pm-table-column-{col}"] button',
     columnHeader: '[id="-column-{col}"] [class="pm-table-column-header"]',
     gearIcon: '[class="jumbotron jumbotron-fluid"]',
-    filterForm: '[class="pm-filter-form"]',
+    //filterForm: '[class="pm-filter-form"]',
     filterFooterBtns: '[class="pm-filter-form"] fieldset',
     tableRequest: '[class="pm-table-container"]',
-    /*new filteringV1
+    //new filteringV1
     openFilter: '[class="pm-table-ellipsis-column"] [class="pm-table-column-header"]',
     threeDots: '.pm-table-ellipsis-column button',
     modalFilter: '[class="pm-filter-form"]',
@@ -15,5 +15,5 @@ export default {
     valueFilter: '[data-cy="pmFilterFormArea"] [data-cy="value{row}"]',
     removeButtonModal: '[data-cy="pmFilterFormArea"] [data-cy="buttonRemove{row}"]',
     addNewFilterButton: '[class="btn pm-filter-form-button btn-light btn-sm"] rect[fill="#D9D9D9"]'
-    */
+    
 };
