7b1100171909c708e2c31e4ce73e14751000ec81
Carlos Camacho <carlos.camacho@processmaker.com>
2026-06-18 19:38:40 -0400

    add new saved search methods

--- pages/savedSearchAPI.js
+++ pages/savedSearchAPI.js
@@ -44,6 +44,46 @@ class SavedSearchAPI {
       });
     });
   }
+  /* payload must be in this format 
+  payload = {
+    "yaml_file_name": "name_savedSearch.yaml",
+    "saved_search_ids": ID_SAVED_SEARCH 
+  }
+  add `SAVED_SEARCH_ADVANCED_CONFIGURATION_ENDPOINT_ENABLED=true` variable in the .env file before use this method
+  */ 
+  createTableAndPolulate(payload){
+    return cy.window().then(win => {
+      return win.ProcessMaker.apiClient.post(
+          `/saved-searches/advanced-configurations/`,
+          payload
+      ).then(response => {
+          return response.data
+      });
+    });
+  }
+
+  /* payload must be in this format 
+  payload = {
+    "saved_search_id": 620,
+    "yaml_file_name": "qa_ss620.yaml",
+    "creation_schedule":{
+        "date":"2026-10-08",
+        "time":"14:00",
+        "timezone":"UTC"
+    }
+  }
+  add `SAVED_SEARCH_ADVANCED_CONFIGURATION_ENDPOINT_ENABLED=true` variable in the .env file before use this method
+  */ 
+  createYamlFile(payload){
+    return cy.window().then(win => {
+      return win.ProcessMaker.apiClient.post(
+          `/saved-searches/advanced-configurations/yaml-files`,
+          payload
+      ).then(response => {
+          return response.data
+      });
+    });
+  }
 }
 
 export default new SavedSearchAPI();
